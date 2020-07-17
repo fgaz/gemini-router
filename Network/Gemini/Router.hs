324e712a7fd8932cbe7ccf91897db40a424d83ea
Francesco Gazzetta <fgaz@fgaz.me>
2020-07-17 17:09:55 +0200

    Add haddocks

--- Network/Gemini/Router.hs
+++ Network/Gemini/Router.hs
@@ -36,6 +36,7 @@ import Control.Monad.Fail (MonadFail(..))
 -- The RouteT monad transformer
 -------------------------------
 
+-- | Represents a way of routing requests through different handlers
 newtype RouteT m a = RouteT { runRouteT :: Request -> [String] -> m (Maybe a) }
 
 type Route = RouteT Identity
@@ -67,10 +68,10 @@ instance Monad m => MonadFail (RouteT m) where
   --or is it possible to somehow directly return Response 42 err mempty? it would require early return... like happstack
   fail _ = empty
 
+-- | 'empty' skips to the next route.
+-- @r1 '<|>' r2@ means go to @r2@ if @r1@ skips
 instance Monad f => Alternative (RouteT f) where
-  -- | Skip to next route
   empty = RouteT $ \_ _ -> pure Nothing
-  -- | @r1 <|> r2@ means go to r2 if r1 skips
   r1 <|> r2 = RouteT $ \req path -> do
     maybe1 <- runRouteT r1 req path
     maybe2 <- runRouteT r2 req path
@@ -80,7 +81,10 @@ instance Monad f => Alternative (RouteT f) where
 -------------------------------
 
 -- MAYBE swap names with runRouteT
-runRouteT' :: (m (Maybe Response) -> IO (Maybe Response)) -> RouteT m Response -> Handler
+-- | Given a @run@ function for the inner 'Monad', make a 'Handler'
+runRouteT' :: (m (Maybe Response) -> IO (Maybe Response)) -- ^ Inner @run@
+           -> RouteT m Response
+           -> Handler
 runRouteT' runM r req = fromMaybe notFound <$> runM (runRouteT r req path)
   where
     notFound = Response 51 "Not found" mempty
@@ -89,36 +93,60 @@ runRouteT' runM r req = fromMaybe notFound <$> runM (runRouteT r req path)
 -- Building Routes
 -------------------------------
 
-end :: Applicative f => RouteT f a -> RouteT f a
+-- | Match on the end of the path
+end :: Applicative f
+    => RouteT f a -- ^ Route to run
+    -> RouteT f a
 end r = RouteT $ \req path -> case path of
   [] -> runRouteT r req path
   _ -> pure Nothing
 
-dir :: Applicative f => String -> RouteT f a -> RouteT f a
+-- | Match on a specific path segment
+dir :: Applicative f
+    => String -- ^ What the segment must match
+    -> RouteT f a -- ^ Route to run on the rest of the path
+    -> RouteT f a
 dir str r = RouteT $ \req path -> case path of
   frag:rest | frag == str -> runRouteT r req rest
   _                       -> pure Nothing
 
 -- TODO use a parsing class
-capture :: Applicative f => (String -> RouteT f a) -> RouteT f a
+-- | Match on an arbitrary path segment, and capture it
+capture :: Applicative f
+        => (String -> RouteT f a) -- ^ Function that takes the segment and
+                                  -- returns the route to run on the rest of
+                                  -- the path
+        -> RouteT f a
 capture f = RouteT $ \req path -> case path of
   frag:rest -> runRouteT (f frag) req rest
   _         -> pure Nothing
 
 -- TODO use a parsing class
-input :: Applicative f => String -> (String -> RouteT f Response) -> RouteT f Response
+-- | Require a query string, by asking the client (code 10) if necessary
+input :: Applicative f
+      => String -- ^ String to return to the client if there is no query string
+      -> (String -> RouteT f Response) -- ^ Function that takes the query string
+                                       -- and returns the route to run on the
+                                       -- rest of the path
+      -> RouteT f Response
 input q f = RouteT $ \req path -> case uriQuery req of
   '?':query -> runRouteT (f $ unEscapeString query) req path
   _         -> pure $ pure $ Response 10 q mempty
 
-optionalInput :: Applicative f => (Maybe String -> RouteT f a) -> RouteT f a
+-- | Capture, if present, the query string
+optionalInput :: Applicative f
+              => (Maybe String -> RouteT f a) -- ^ Function that takes the
+                                              -- query string (if present) and
+                                              -- returns the route to run on
+                                              -- the rest of the path
+              -> RouteT f a
 optionalInput f = RouteT $ \req path -> case uriQuery req of
   '?':query -> runRouteT (f $ Just $ unEscapeString query) req path
   _         -> runRouteT (f Nothing)                       req path
 
-custom :: (Request
-       -> [String] -- Remaining path
-       -> m (Maybe a) -- A Nothing makes the request fall through
-       ) -> RouteT m a
+-- | Build custom routes. Takes a function that takes the request and the
+-- remaining path segments and returns the result. A 'Nothing' makes the
+-- request fall through to the next route
+custom :: (Request -> [String] -> m (Maybe a)) -> RouteT m a
 custom = RouteT
 
